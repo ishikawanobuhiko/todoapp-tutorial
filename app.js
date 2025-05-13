d49c36b035d7951789030b7041f227cbadcdf33d
NobuIshikawat <ishikawa@cilas.net>
2025-05-13 15:26:38 +0900

    色の修正を行いました

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ $("input").val("");
 $(document).on("change", "input[type=checkbox]", function () {
 if($(this).is(":checked")) {
     $(this).parent().css("text-decoration", "line-through");
+    $(this).parent().css("color", "#ccc");
 } else {
 $(this).parent().css("text-decoration", "none");
+    $(this).parent().css("color", "#000");
+
 }
 
 
