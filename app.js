f6c48e34a2d02e07d7ad40d2412b5d9469d5d0d3
NobuIshikawat <ishikawa@cilas.net>
2025-05-13 15:16:15 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const inputTodo = $("input").val();
 $("#todoList").append("<li><input type=checkbox>" + inputTodo + "</>");
 $("input").val("");
 });
+
+$(document).on("change", "input[type=checkbox]", function () {
+if($(this).is(":checked")) {
+    $(this).parent().css("text-decoration", "line-through");
+} else {
+$(this).parent().css("text-decoration", "none");
+}
+
+
+});
